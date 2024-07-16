e044fd4328c8f29469e33d1e5b4bc3780d016279
marciopalves <marciopalves89@gmail.com>
2024-07-16 16:29:18 -0300

    Configurando o Sequelize

---
+++ config/sequelizeCli.js
@@ -0,0 +1,10 @@
+module.exports = {
+    development: {
+        dialect: 'postgres',
+        host: 'localhost',
+        port: '5432',
+        database: 'ead_development',
+        username: 'root',
+        password: '123456'
+    }
+}
